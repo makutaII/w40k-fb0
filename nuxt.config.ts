b401b0556bd034d2cd3246c5bdeaabc9ee6f3ba8
Makuta <igovano2@gmail.com>
2024-01-25 21:12:01 +0300

    rutine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
-  ssr:true,
+  ssr:false,
   devtools: { enabled: true },
   //...
   build: {
