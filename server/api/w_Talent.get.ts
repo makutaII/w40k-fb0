c1489fff558c3da63706c1e5a744852d26a7f3b0
Makuta <igovano2@gmail.com>
2024-01-25 23:44:39 +0300

    f

--- server/api/w_Talent.get.ts
+++ server/api/w_Talent.get.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async () => {  
-  return await prisma.w_Talent.findMany(
+  return await prisma.w_talent.findMany(
   //  {include: { requirements: true }, }
   );
 });
