98485c8184b475d43d8aaa49422fb5e9569fa51e
Makuta <igovano2@gmail.com>
2024-02-06 14:54:17 +0300

    add descriptions on pages

--- pages/specializations.vue
+++ pages/specializations.vue
@@ -1,69 +1,68 @@
 <template>
-    <v-container>
-        <h1 class="ma-lg-9">Специализации</h1>
+  <v-container>
+    <h1 class="ma-lg-9">Специализации</h1>
+    <v-row>
+      <div>
+        Специализации отражают концентрацию определённой области при применении персонажем умения.
+      </div></v-row
+    >
 
-        <v-row>
-            <v-col v-for="(spec, i) in specsD" :key="i">
-                <v-expansion-panels>
+    <v-row>
+      <v-col v-for="(spec, i) in specsD" :key="i">
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <!-- class="bg-surface-variant" -->
+            <v-expansion-panel-title class="pa-2">
+              <v-col cols="9" class="text-capitalize" style="min-width: 210px">
+                {{ spec.name }}
+              </v-col>
 
-                    <v-expansion-panel >
-                        <!-- class="bg-surface-variant" -->
-                        <v-expansion-panel-title class="pa-2">
-                            <v-col cols="9" class="text-capitalize" style="min-width: 210px;">
-                                {{ spec.name }}
+              <v-divider
+                :thickness="20"
+                class="border-opacity-0"
+                vertical
+              ></v-divider>
+              <!-- <v-btn disabled="" size="23" icon="mdi-account" :to="`/talents/${talent.id}`"> -->
 
-                            </v-col>
+              <!-- </v-btn> -->
+            </v-expansion-panel-title>
 
-                            <v-divider :thickness="20" class="border-opacity-0" vertical></v-divider>
-                            <!-- <v-btn disabled="" size="23" icon="mdi-account" :to="`/talents/${talent.id}`"> -->
+            <v-expansion-panel-text>
+              <!-- <div v-if="talent.requirements[0]" class="text-subtitle-2 font-weight-bold mt-4">Требования:</div> -->
 
-                            <!-- </v-btn> -->
-                        </v-expansion-panel-title>
-
-                        <v-expansion-panel-text>
-
-                            <!-- <div v-if="talent.requirements[0]" class="text-subtitle-2 font-weight-bold mt-4">Требования:</div> -->
-
-<!--                            
+              <!--                            
                             <div class="text-subtitle-2" v-for="(req, i) in talent.requirements">
                                 {{ req.name }}
                             </div> -->
-                              <v-divider class="mb-3"></v-divider>
-                            <p class="mb-5">
-                                {{ spec.description }}
-                            </p>
-                        </v-expansion-panel-text>
-
-                    </v-expansion-panel>
-                </v-expansion-panels>
-
-            </v-col>
-        </v-row>
-
-    </v-container>
+              <v-divider class="mb-3"></v-divider>
+              <p class="mb-5">
+                {{ spec.description }}
+              </p>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-        <!-- ================================= -->
+<!-- ================================= -->
 
 <script setup>
-
 //================
 //получаем данные
 
 //рабочий вариант для локалки
-const specsD = ref(null)
-specsD.value = await getSpecialization()
+const specsD = ref(null);
+specsD.value = await getSpecialization();
 //console.log(talentsD.value)
-async function getSpecialization() { return await $fetch('/api/w_specialization') } 
-
-
-
+async function getSpecialization() {
+  return await $fetch("/api/w_specialization");
+}
 </script>
 <script>
 export default {
-    data: () => ({
-
-    }),
-}
+  data: () => ({}),
+};
 </script>
 
 <style scoped></style>
--- pages/talents/index.vue
+++ pages/talents/index.vue
@@ -1,68 +1,80 @@
 <template>
-    <v-container>
-        <h1 class="ma-lg-9">Таланты</h1>
+  <v-container>
+    <h1 class="ma-lg-9">Таланты</h1>
+    <v-row>
+      <div>
+        Таланты отражают собственные уникальные способности, причуды и хитрые
+        трюки
+      </div></v-row
+    >
 
-        <v-row>
-            <v-col v-for="(talent, i) in talentsD" :key="i">
-                <v-expansion-panels>
+    <v-row>
+      <v-col v-for="(talent, i) in talentsD" :key="i">
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <!-- class="bg-surface-variant" -->
+            <v-expansion-panel-title class="pa-2">
+              <v-col cols="9" class="text-capitalize" style="min-width: 210px">
+                {{ talent.name }}
+              </v-col>
 
-                    <v-expansion-panel >
-                        <!-- class="bg-surface-variant" -->
-                        <v-expansion-panel-title class="pa-2">
-                            <v-col cols="9" class="text-capitalize" style="min-width: 210px;">
-                                {{ talent.name }}
+              <v-divider
+                :thickness="20"
+                class="border-opacity-0"
+                vertical
+              ></v-divider>
+              <!-- <v-btn disabled="" size="23" icon="mdi-account" :to="`/talents/${talent.id}`"> -->
 
-                            </v-col>
+              <!-- </v-btn> -->
+            </v-expansion-panel-title>
 
-                            <v-divider :thickness="20" class="border-opacity-0" vertical></v-divider>
-                            <!-- <v-btn disabled="" size="23" icon="mdi-account" :to="`/talents/${talent.id}`"> -->
+            <v-expansion-panel-text>
+              <div
+                v-if="talent.requirements[0]"
+                class="text-subtitle-2 font-weight-bold mt-4"
+              >
+                Требования:
+              </div>
 
-                            <!-- </v-btn> -->
-                        </v-expansion-panel-title>
+              <div
+                class="text-subtitle-2"
+                v-for="(req, i) in talent.requirements"
+              >
+                {{ req.name }}
+              </div>
+              <v-divider class="mb-3"></v-divider>
+              <p class="mb-5">
+                {{ talent.description }}
+              </p>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
 
-                        <v-expansion-panel-text>
-
-                            <div v-if="talent.requirements[0]" class="text-subtitle-2 font-weight-bold mt-4">Требования:</div>
-
-                           
-                            <div class="text-subtitle-2" v-for="(req, i) in talent.requirements">
-                                {{ req.name }}
-                            </div>
-                              <v-divider class="mb-3"></v-divider>
-                            <p class="mb-5">
-                                {{ talent.description }}
-                            </p>
-                        </v-expansion-panel-text>
-
-                    </v-expansion-panel>
-                </v-expansion-panels>
-
-            </v-col>
-        </v-row>
-
-        <!-- ================================= -->
-        <!-- <h3  class = "ma-lg-9">Control</h3>
+    <!-- ================================= -->
+    <!-- <h3  class = "ma-lg-9">Control</h3>
         <div class="mb-16">
             <div v-for="t in talentsD">
                 <NuxtLink :to="`/talents/${t.id}`">{{ t.title }}</NuxtLink>
             </div>
 
         </div> -->
-    </v-container>
+  </v-container>
 </template>
-        <!-- ================================= -->
+<!-- ================================= -->
 
 <script setup>
-
 //================
 //получаем данные
 
 //рабочий вариант для локалки
-const talentsD = ref(null)
-talentsD.value = await getTalents()
+const talentsD = ref(null);
+talentsD.value = await getTalents();
 //console.log(talentsD.value)
-async function getTalents() { return await $fetch('/api/w_talent') } 
-
+async function getTalents() {
+  return await $fetch("/api/w_talent");
+}
 
 //----  другой шаблон страницы
 // definePageMeta({
@@ -79,14 +91,11 @@ async function getTalents() { return await $fetch('/api/w_talent') }
 // ------------------------------
 
 //import { ref } from 'vue'
-
 </script>
 <script>
 export default {
-    data: () => ({
-
-    }),
-}
+  data: () => ({}),
+};
 </script>
 
 <style scoped></style>
